22b599c27b914c30e4c94f5c39dc60b107263cf8
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-02-19 21:57:44 +0100

    Added THArrayViewCell

---
+++ THArrayView/THArrayViewCell.h
@@ -0,0 +1,13 @@
+//
+//  THArrayViewCell.h
+//  RefurbMe
+//
+//  Created by Tanguy Hélesbeux on 19/02/2014.
+//  Copyright (c) 2014 RefurbMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THArrayViewCell : UIView
+
+@end
---
+++ THArrayView/THArrayViewCell.m
@@ -0,0 +1,31 @@
+//
+//  THArrayViewCell.m
+//  RefurbMe
+//
+//  Created by Tanguy Hélesbeux on 19/02/2014.
+//  Copyright (c) 2014 RefurbMe. All rights reserved.
+//
+
+#import "THArrayViewCell.h"
+
+@implementation THArrayViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
